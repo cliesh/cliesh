6d41d25898be0d3b79a4c313f96d770b6d475afb
anonysoul <i@anonysoul.com>
2022-09-20 21:56:38 +0800

    refactor: duration pipe maximum support for one year

--- src/app/pipe/duration-time.pipe.ts
+++ src/app/pipe/duration-time.pipe.ts
@@ -10,13 +10,19 @@ export class DurationTimePipe implements PipeTransform {
     if (durationMinutes < 1) {
       return "less then a minute";
     } else if (durationMinutes < 60) {
-      return `${durationMinutes} minutes`;
+      return durationMinutes === 1 ? "1 minute ago" : `${durationMinutes} minutes ago`;
     } else if (durationMinutes < 60 * 24) {
-      return `about ${Math.floor(durationMinutes / 60)} hours`;
+      const durationHours = Math.floor(durationMinutes / 60);
+      return durationHours === 1 ? "1 hour ago" : `${durationHours} hours ago`;
     } else if (durationMinutes < 60 * 24 * 30) {
-      return `about ${Math.floor(durationMinutes / 60 / 24)} days`;
+      const durationDays = Math.floor(durationMinutes / 60 / 24);
+      return durationDays === 1 ? "1 day ago" : `${durationDays} days ago`;
+    } else if (durationMinutes < 60 * 24 * 30 * 12) {
+      const durationMonths = Math.floor(durationMinutes / 60 / 24 / 30);
+      return durationMonths === 1 ? "1 month ago" : `${durationMonths} months ago`;
     }
-    // more than a month
+
+    // more than one year
     return "long long ago";
   }
 }
